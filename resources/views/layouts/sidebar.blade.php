e068335bb431ff4f4d8b6e42bce45a64987d930a
Chris Landon <chris@automashion.com>
2019-10-13 10:08:17 -0700

    Init laravel+tailwind+auth

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,17 @@
+<div class="bg-gray-900 h-full w-56 fixed">
+    <ul class="list-none">
+        <li class="mb-0">
+            <a href="{{ route('home') }}" class="text-gray-500 text-sm px-3 py-3 hover:text-gray-200 hover:bg-gray-800 block">Home</a>
+        </li>
+        <li class="mb-0">
+            <a href="#" class="text-gray-500 text-sm px-3 py-3 hover:text-gray-200 hover:bg-gray-800 block">Test</a>
+        </li>
+        <li class="mb-0">
+            <a href="#" class="text-gray-500 text-sm px-3 py-3 hover:text-gray-200 hover:bg-gray-800 block">Test</a>
+        </li>
+        <li class="mb-0">
+            <a href="#" class="text-gray-500 text-sm px-3 py-3 hover:text-gray-200 hover:bg-gray-800 block">Test</a>
+        </li>
+
+    </ul>
+</div>
